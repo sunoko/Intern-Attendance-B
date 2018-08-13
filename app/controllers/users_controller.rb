d929f5d547ea62fe8a2cccea5483b99717a14552
EC2 Default User <ec2-user@ip-172-31-47-28.us-east-2.compute.internal>
2018-08-13 19:37:28 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-  end
+   end
 
   def edit
     @user = User.find(params[:id])
@@ -54,15 +55,6 @@ class UsersController < ApplicationController
     end
     
     # beforeアクション
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
     
     # 正しいユーザーかどうか確認
     def correct_user
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PasswordResetsTest < ActionDispatch::IntegrationTest
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
