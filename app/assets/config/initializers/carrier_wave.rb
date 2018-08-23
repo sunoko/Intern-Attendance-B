acb71f466d95d03d6ceabf1f504ade510c39992c
EC2 Default User <ec2-user@ip-172-31-47-28.us-east-2.compute.internal>
2018-08-23 11:38:59 +0000

    master

--- app/assets/config/initializers/carrier_wave.rb
+++ app/assets/config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAIFYVKZK7IDQAZY7A'],
-      :aws_secret_access_key => ENV['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']
-    }
-    config.fog_directory     =  ENV['kaneko']
-  end
-end
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       # Amazon S3用の設定
+#       :provider              => 'AWS',
+#       :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
+#       :aws_access_key_id     => ENV['AKIAIFYVKZK7IDQAZY7A'],
+#       :aws_secret_access_key => ENV['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']
+#     }
+#     config.fog_directory     =  ENV['kaneko']
+#   end
+# end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,11 +2,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
