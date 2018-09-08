bd8a746471232a124a17dbe20c849aa45da2c71b
EC2 Default User <ec2-user@ip-172-31-47-28.us-east-2.compute.internal>
2018-09-08 05:39:47 +0000

    my-portfolio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  
+  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'kaneko-sample-app.herokuapp.com'
@@ -78,6 +81,9 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+  
+  
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
