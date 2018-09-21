62c77cc758c1bad7e45e4e018a5efcbf1a2a7374
EC2 Default User <ec2-user@ip-172-31-47-28.us-east-2.compute.internal>
2018-09-21 14:58:15 +0000

    Correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'kaneko-sample-app.herokuapp.com'
+  host = 'kenko-portfolio.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
