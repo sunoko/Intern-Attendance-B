37de3034585c47d1f9cf3c39d901664522cff14b
EC2 Default User <ec2-user@ip-172-31-23-121.us-east-2.compute.internal>
2018-11-05 05:36:23 +0000

    atten_times テーブルへuser_idカラムを追加

--- db/migrate/20181012150937_create_atten_times.rb
+++ db/migrate/20181012150937_create_atten_times.rb
@@ -3,7 +3,8 @@ class CreateAttenTimes < ActiveRecord::Migration[5.1]
     create_table :atten_times do |t|
       t.time :arrival_time
       t.time :departure_time
-
+      t.integer :user_id
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181012150937) do
+ActiveRecord::Schema.define(version: 20181025132533) do
 
   create_table "atten_times", force: :cascade do |t|
     t.time "arrival_time"
     t.time "departure_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "microposts", force: :cascade do |t|
